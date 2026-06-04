8fca023ed5637e2c21ac5900e505869482873805
Rodney Tai <rodneytai97@gmail.com>
2026-06-04 23:36:15 +0800

    Initial FastAPI learning

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from mcp.server.fastmcp import FastMCP 
+
+mcp = FastMCP("core-rag-service")
+
+@mcp.tool()
+async def query_knowledge_base(query: str) ->str:
+    return f"[Mock Response] Found standard documentaion reference for: '{query}'"
+
+
+app = FastAPI(title="Repo 1: Protocol Core Data Service")
+
+@app.get("/health")
+async def health_check():
+    return{
+        "status": "Healthy",
+        "protocol": "Dual-REST-MCP",
+        "engine": "FastAPI + FastMCP"    
+    }
+
+app.mount("/", mcp.sse_app())
+
+# def main():
+#     print("Hello from core-rag-service!")
+
+
+# if __name__ == "__main__":
+#     main()
